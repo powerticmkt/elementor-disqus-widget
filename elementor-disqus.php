bd3a4a4097c32ac530ef8e90cc32be12eb491efa
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2017-03-27 11:10:50 -0300

    Bump to 0.2.0

--- elementor-disqus.php
+++ elementor-disqus.php
@@ -3,7 +3,7 @@
  * Plugin Name: Elementor Disqus Widget
  * Description: Elementor widget to show disqus on your pages.
  * Plugin URI:  https://www.powertic.com.br/
- * Version:     0.1.0
+ * Version:     0.2.0
  * Author:      Powertic
  * Author URI:  https://www.powertic.com/br/
  * Text Domain: elementor-disqus
@@ -33,7 +33,7 @@ function elementor_disqus_load() {
 	if ( ! version_compare( ELEMENTOR_VERSION, $elementor_version_required, '>=' ) ) {
 		add_action( 'admin_notices', 'elementor_disqus_fail_load_out_of_date' );
 		return;
-	}	
+	}
 
 	// Require the main plugin file
 	require( __DIR__ . '/plugin.php' );
